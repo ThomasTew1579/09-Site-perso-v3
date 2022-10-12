3907bf72686757757f8ecc40801ca58a3bc8a80a
Thomas <pasquet.thomas69@gmail.com>
2022-10-12 16:15:47 +0200

    finition

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,52 @@
+let burger = document.getElementById("burger");
+let header = document.querySelector("header")
+let nav = document.querySelector("nav");
+let liensNav = nav.querySelectorAll("a");
+
+function activeMenu () {
+    if (header.classList.contains("active")) {
+        header.classList.replace("active", "desactive");
+    } else {
+        header.classList.replace("desactive", "active");
+    }
+  }
+
+burger.addEventListener("click", () => {
+    activeMenu()
+})
+
+liensNav[0].addEventListener("click", () => {
+    activeMenu()
+})
+
+liensNav[1].addEventListener("click", () => {
+    activeMenu()
+})
+
+liensNav[2].addEventListener("click", () => {
+    activeMenu()
+})
+
+liensNav[3].addEventListener("click", () => {
+    activeMenu()
+})
+
+
+let cv = document.querySelector(".arrowWrapper");
+
+
+cv.addEventListener("mouseover", () => {
+    console.log("ping")
+  cv.classList.add("active");
+});
+cv.addEventListener("mouseleave", () => {
+  cv.classList.remove("active");
+}); 
+
+
+
+
+
 let homePhoto = document.querySelector(".home_photo")
 let home = document.getElementById("home")
 
@@ -14,7 +63,7 @@ let article = document.querySelectorAll("article")
 
 
 
-function active (id, scroll){
+function activecroll (id, scroll){
     if (window.scrollY < scroll) {
         id.classList.replace("active", "desactive");
     } else {
@@ -23,15 +72,21 @@ function active (id, scroll){
 }
 
 document.addEventListener("scroll", () => {
-    console.log(window.scrollY)
-    
-    active(about, 200)
+    // console.log(window.scrollY)
+    activecroll(about, 250);
 
-    active(article[0], 900)
-    active(article[1], 1400)
-    active(article[2], 1900)
-    active(article[3], 2400)
-    active(article[4], 2900)
-
+    if(window.innerWidth < 600 ){
+        activecroll(article[0], 1250);
+        activecroll(article[1], 1750);
+        activecroll(article[2], 2250);
+        activecroll(article[3], 2750);
+        activecroll(article[4], 3250);
+    } else {
+        activecroll(article[0], 800);
+        activecroll(article[1], 1300);
+        activecroll(article[2], 1800);
+        activecroll(article[3], 2300);
+        activecroll(article[4], 2800);
+    }
     
 })
